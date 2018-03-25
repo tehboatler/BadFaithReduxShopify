f07e97d6a7a93d4db7e46fa1f32e4f990de59e69
jb <jandrewbutler.93@gmail.com>
2018-03-25 20:42:38 +1100

    Added Header Transition Animations: Web Animations API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import reactDOM from 'react-dom';
 
 import HeaderPhone from '../img/BadFaithHeaderPhone.png';
 
@@ -7,9 +10,10 @@ const HeaderContainer = styled.div`
   padding-top: 7.5vh;
   display: flex;
   justify-content: center;
-  height: 70vh;
+  height: ${({ isHome }) => (isHome ? '70vh' : '0vh')};
   width: 100%;
   background-color: #222;
+  overflow: hidden;
 `;
 
 // 422 * 749
@@ -39,16 +43,16 @@ const BadFaithLogo = styled.h1`
   align-self: center;
   color: black;
   z-index: 1;
- bottom: 5vh;
+  bottom: 5vh;
 `;
 
 const BadFaithLogoTagline = styled.h2`
-position: absolute;
-align-self: center;
-color: black;
-z-index: 1;
-bottom: 0;
-`
+  position: absolute;
+  align-self: center;
+  color: black;
+  z-index: 1;
+  bottom: 0;
+`;
 
 const PhoneImage = styled.img`
   position: absolute;
@@ -57,18 +61,47 @@ const PhoneImage = styled.img`
   height: 70vh;
 `;
 
-const Header = () => {
-  return (
-    <HeaderContainer>
-      <PhoneImageWrapper>
-        <BadFaithLogoContainer>
-          <BadFaithLogo>BadFaith</BadFaithLogo>
-          <BadFaithLogoTagline>Not just a case.</BadFaithLogoTagline>
-        </BadFaithLogoContainer>
-        <PhoneImage src={HeaderPhone} />
-      </PhoneImageWrapper>
-    </HeaderContainer>
-  );
-};
+export class Header extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (location.pathname !== prevProps.location.pathname) {
+      if (location.pathname === '/') {
+        this.header.animate([{ height: '0vh' }, { height: '70vh' }], {
+          duration: 300,
+          fill: 'forwards',
+          easing: 'cubic-bezier(0.86, 0, 0.07, 1)'
+        });
+      } else {
+        if (prevProps.location.pathname === '/') {
+          this.header.animate([{ height: '70vh' }, { height: '0vh' }], {
+            duration: 300,
+            fill: 'forwards',
+            easing: 'cubic-bezier(0.86, 0, 0.07, 1)'
+          });
+        }
+      }
+    }
+  }
 
-export default Header;
+  render() {
+    return (
+      <HeaderContainer
+        isHome={location.pathname === '/'}
+        ref={c => (this.header = reactDOM.findDOMNode(c))}
+      >
+        <PhoneImageWrapper>
+          <BadFaithLogoContainer>
+            <BadFaithLogo>BadFaith</BadFaithLogo>
+            <BadFaithLogoTagline>Not just a case.</BadFaithLogoTagline>
+          </BadFaithLogoContainer>
+          <PhoneImage src={HeaderPhone} />
+        </PhoneImageWrapper>
+      </HeaderContainer>
+    );
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
