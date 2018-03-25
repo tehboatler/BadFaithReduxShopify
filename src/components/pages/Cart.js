7601b945388294644eb11be3dffaa73c41e8f4ee
jb <jandrewbutler.93@gmail.com>
2018-03-25 20:42:21 +1100

    Set-up Cart UI

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -7,7 +7,42 @@ import { withRouter } from 'react-router-dom';
 const RootContainer = styled.div`
   height: 30vh;
   width: 100%;
-  background-color: red;
+  // background-color: red;
+  padding: 0vw 10vw;
+`;
+
+const CartTitleWrapper = styled.div`
+  height: auto;
+  width: 10vw;
+  // background-color: grey;
+`;
+
+const CartTitle = styled.h1`
+  color: black;
+  font-size: 2rem;
+`;
+
+const OrderSummaryTitle = styled.h1`
+  color: black;
+  font-size: 1.5rem;
+`;
+
+const CartSeparatorLarge = styled.div`
+  height: 1vh;
+  width: 80vw;
+  background-color: black;
+`;
+
+const CartSeparatorSmall = styled.div`
+  height: 0.2vh;
+  width: 80vw;
+  background-color: black;
+`;
+
+const CartItem = styled.div`
+  height: 10vh;
+  width: 80vw;
+  background-color: lightblue;
 `;
 
 export class Cart extends Component {
@@ -19,8 +54,19 @@ export class Cart extends Component {
     );
   };
   renderEmpty = () => {
+    const { cart } = this.props;
     return (
       <RootContainer>
+        <CartTitleWrapper>
+          <CartTitle>Cart</CartTitle>
+        </CartTitleWrapper>
+        <CartSeparatorLarge />
+        <OrderSummaryTitle>Order Summary</OrderSummaryTitle>
+        <CartSeparatorSmall />
+        {cart.map(CartItems => {
+          return <CartItem />;
+        })}
+        <CartSeparatorSmall />
         <div />
       </RootContainer>
     );
