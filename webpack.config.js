62b2d1e979087a9f52a00ba9447dbacded487b86
jb <jandrewbutler.93@gmail.com>
2018-03-30 22:50:30 +1100

    webpack config build fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var path = require('path');
 const webpack = require('webpack');
 module.exports = {
   entry: './src/app.js',
-  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
