719b5c8a80f036ebaf87fbbb809396de889c7606
jb <jandrewbutler.93@gmail.com>
2018-03-31 11:47:19 +1100

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: ["babel-polyfill", './src/app.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
@@ -13,20 +13,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.es6$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2017']
-        }
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2017', 'stage-1'],
-          plugins: ['graphql-js-client-transform']
+          presets: ['react', 'env'],
+          plugins: ['graphql-js-client-transform',"transform-object-rest-spread", "transform-class-properties" ]
         }
       },
       {
@@ -44,11 +36,25 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new UglifyJSPlugin()
-  ],
+  // plugins: [
+  //   // This helps ensure the builds are consistent if source hasn't changed:
+  //   new webpack.optimize.OccurrenceOrderPlugin(),
+  //   // Try to dedupe duplicated modules, if any:
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress: {
+  //       screw_ie8: true, // React doesn't support IE8
+  //       warnings: false
+  //     },
+  //     mangle: {
+  //       screw_ie8: true
+  //     },
+  //     output: {
+  //       comments: false,
+  //       screw_ie8: true
+  //     }
+  //   }),
+  // ],
   devServer: {
     historyApiFallback: true
   }
 };
-
